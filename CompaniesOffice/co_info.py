7e643eba8f2c4258ced7b53d20a4cf6c08f1b597
Stephen Zou <zou.stephen@gmail.com>
2018-02-14 22:05:57 +1300

    use BeautifulSoup to parse result

--- CompaniesOffice/co_info.py
+++ CompaniesOffice/co_info.py
@@ -1,9 +1,17 @@
 import requests
+import json
+from bs4 import BeautifulSoup
 
-company_name = input("Please input the company's name: ")
+company_name = input("Please input the company's name: ").replace(' ', '+')
 
-search_url = "https://companies-register.companiesoffice.govt.nz/search/?keyword=" + company_name + "+&category=companies&filters%5B%5D=REGISTERED&filters%5B%5D=REMOVE&filters%5B%5D=EXTERNAL_ADMINISTRATION"
-
+# generate a url to send request to receive company's ID
+search_url = "https://companies-register.companiesoffice.govt.nz/search/?keyword=" + company_name + """&category=companies&
+filters%5B%5D=REGISTERED&
+filters%5B%5D=REMOVE&
+filters%5B%5D=EXTERNAL_ADMINISTRATION&
+ajax=true
+"""
 search_result = requests.get(search_url)
 
 print(search_result.text)
+
